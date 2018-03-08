75750da5988aa63df78a5a61fc348e1404d94cc3
Alex Penketh <alex@apenketh.com>
2018-03-08 22:38:25 +0800

    Update add-ansible-user.sh

--- startup-scripts/add-ansible-user.sh
+++ startup-scripts/add-ansible-user.sh
@@ -14,5 +14,5 @@ touch /home/ansible/.ssh/authorized_keys
 chmod 600 /home/ansible/.ssh/authorized_keys
 chown -R ansible:ansible /home/ansible/.ssh/
 
-# Add The Pub Key In For Ansible Access
-echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDIDlsVrbhTgr7edBgsx3RYHhSWxPbN/RgoqMGfdFjVq6zJ9pDva5/zoPuKEsUHRcRDiBFY1ohR0693oNFSjmgHaWHjQFo42n954HIopoUlAkgrhmj3UsLK0WewtHCDB9Of04HGKoRfyxvXMvdteBeUyb3+hqmmy/rjrZQPuq7Psi451jfKnQoWT/gfh9wN09SIS9JWXx/gxkDZnj30miKUxwSfAxro0ZJ0R2EE0AySnwLmwV4uwdCHMuDMTmJjAV9LDbvDuAiugwo5Umrng4f2WGz6/gxy6J+25jcDcgi33qnonPXFvsBNuAuoyL/BxgtzJwsTK4HLsJQLjfG4g/aN root@ovh-twr01" >> /home/ansible/.ssh/authorized_keys
+# Add The Pub Key In For Ansible Access - Makre sure to replace this with your key!
+echo "{replace_with_pub_key}" >> /home/ansible/.ssh/authorized_keys
